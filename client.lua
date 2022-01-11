92cfa2b8cfceb95d675e624db6c3b27fdc78266b
Chxttr <49021789+Chxttr@users.noreply.github.com>
2022-01-11 23:36:47 +0100

    Remove whitespaces

--- client.lua
+++ client.lua
@@ -1,48 +1,29 @@
-
----------------
--- Functions --
----------------
-
 local QBCore = exports['qb-core']:GetCoreObject()
--- (server side script)
--- Registers a command named 'ping'.
 local spawned = false
 --[[ RegisterCommand("cats", function()
     spawnDOGS()
-
 end, false ) ]]
 CreateThread(function()
     local spot1 = CircleZone:Create(vector3(-580.54, -1064.7, 22.79), 10.0, {
         name="spot1",
         debugPoly=false,
     })
-    
-
-
-		while true do
-			Wait(3000)
-			local plyPed = PlayerPedId()
-			local coord = GetEntityCoords(plyPed)
-			if spot1:isPointInside(coord)then
-                if not spawned then
-                    spawnDOGS()
-                end
-             
---[[ 			else
-                 print("despawn function not done yett") ]]
+	while true do
+		Wait(3000)
+		local plyPed = PlayerPedId()
+		local coord = GetEntityCoords(plyPed)
+		if spot1:isPointInside(coord)then
+			if not spawned then
+				spawnDOGS()
 			end
-
-
-		 
-
+		-- else
+		--     print("despawn function not done yett")
 		end
-
-		   
-
+	end
 end)
 
 function spawnDOGS()
-spawned = true
+	spawned = true
     for key, value in pairs(Config.Cats) do
         local hash = GetHashKey('a_c_cat_01')
         RequestModel(hash)
@@ -51,7 +32,6 @@ spawned = true
         end
         if value.sitting  == true then
             ped = CreatePed(28, hash, value.coords.x, value.coords.y, value.coords.z - 0.9, value.coords.w, false, true)
-
             DoRequestAnimSet('creatures@cat@amb@world_cat_sleeping_ground@idle_a')
             TaskPlayAnim(ped, 'creatures@cat@amb@world_cat_sleeping_ground@idle_a', 'idle_a' ,8.0, -8, -1, 1, 0, false, false, false)
             SetPedCanBeTargetted(ped, false)
@@ -59,15 +39,11 @@ spawned = true
             SetBlockingOfNonTemporaryEvents(ped, true)
         else
             ped = CreatePed(28, hash, value.coords.x, value.coords.y, value.coords.z - 1.0, value.coords.w, false, true)
-
-
             SetPedCanBeTargetted(ped, false)
             SetEntityAsMissionEntity(ped, true,true)
             TaskWanderStandard(ped, 0, 0)
             SetBlockingOfNonTemporaryEvents(ped, true)
         end
-
-
     end
 end
 
